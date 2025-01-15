54f969f95e33612591e3ffee1d4213a58b49cff5
Liryuu's <107817909+Liryuuu@users.noreply.github.com>
2025-01-15 17:57:18 +0700

    Update fxmanifest.lua

--- lyneix_adrenalinerush/fxmanifest.lua
+++ lyneix_adrenalinerush/fxmanifest.lua
@@ -1,10 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
-
+lua54 'yes'
 author 'lyneix_team'
 description 'lyneix_aDrenalinerush'
 version '1.3.0'
-shared_script 'config.lua'
+shared_script {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
 client_scripts {
     'client/client.lua'
 }
